5395a8964d89c7867cb3a04b09e0e3316820672e
laney0808 <122271269+laney0808@users.noreply.github.com>
2023-06-07 15:04:25 +0800

    Delete app directory

--- app/(home)/_layout.jsx
+++
@@ -1,9 +0,0 @@
-import { Tabs } from "expo-router";
-
-export default function HomeScreen() {
-    return (
-        <Tabs>
-            <Tabs.Screen name="index" options={{ title: "Contents" }} />
-        </Tabs>
-    );
-}
